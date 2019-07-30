2bda45093569b78403c04f7232b59e84e5503dba
choi <rodumani1012@naver.com>
2019-07-30 16:56:25 +0900

    0730 오후

--- forest/src/main/java/com/my/four/model/dao/AnimalShelterListDaoImpl.java
+++ forest/src/main/java/com/my/four/model/dao/AnimalShelterListDaoImpl.java
@@ -1,15 +1,35 @@
 package com.my.four.model.dao;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.my.four.model.dto.AnimalShelterListDto;
 
+@Repository
 public class AnimalShelterListDaoImpl implements AnimalShelterListDao {
 
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	@Transactional
 	@Override
 	public int insert(List<AnimalShelterListDto> dtos) {
-		// TODO Auto-generated method stub
-		return 0;
+
+		int res = 0;
+		
+		Map<String, List<AnimalShelterListDto>> map = new HashMap<String, List<AnimalShelterListDto>>();
+		map.put("list", dtos);
+		
+		res = sqlSession.insert(namespace + "insertList", map);
+		
+		return res;
 	}
 
 }
