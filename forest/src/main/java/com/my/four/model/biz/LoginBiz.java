3403a9eff27ff360b87842927cc044bdb56b8955
choi <rodumani1012@naver.com>
2019-07-30 18:37:21 +0900

    포맷

--- forest/src/main/java/com/my/four/model/biz/LoginBiz.java
+++
@@ -1,9 +0,0 @@
-package com.my.four.model.biz;
-
-import com.my.four.model.dto.LoginDto;
-
-public interface LoginBiz {
-	
-	public int joinMember(LoginDto dto);  
-	public boolean idChk(String id);
-}
--- forest/src/main/java/com/my/four/model/biz/MailService.java
+++
@@ -1,6 +0,0 @@
-package com.my.four.model.biz;
-
-public interface MailService {
-
-	public boolean send(String subject,String text,String from,String to,String filepath);
-}
